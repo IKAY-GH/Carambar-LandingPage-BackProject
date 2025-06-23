37a6a2d708c8282fd64ae432447f4aa91f8b8fc2
CAYUELA Annick <annickcayuela.ac@gmail.com>
2025-06-23 23:54:10 +0200

    automatic jokes

--- src/app.js
+++ src/app.js
@@ -46,8 +46,28 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Synchroniser la BDD et lancer le serveur
 const port = process.env.PORT;
 
-sequelize.sync().then(() => {
+const blagues = [
+  { question: "Quelle est la femelle du hamster ?", answer: "L’Amsterdam" },
+  { question: "Que dit un oignon quand il se cogne ?", answer: "Aïe" },
+  { question: "Quel est l'animal le plus heureux ?", answer: "Le hibou, parce que sa femme est chouette." },
+  { question: "Pourquoi le football c'est rigolo ?", answer: "Parce que Thierry en rit" },
+  { question: "Quel est le sport le plus fruité ?", answer: "La boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes." },
+  { question: "Que se fait un Schtroumpf quand il tombe ?", answer: "Un Bleu" },
+  { question: "Quel est le comble pour un marin ?", answer: "Avoir le nez qui coule" },
+  { question: "Qu'est ce que les enfants usent le plus à l'école ?", answer: "Le professeur" },
+  { question: "Quel est le sport le plus silencieux ?", answer: "Le para-chuuuut" },
+  { question: "Quel est le comble pour un joueur de bowling ?", answer: "C’est de perdre la boule" }
+];
+
+sequelize.sync().then(async () => {
   console.log('Database synced');
+
+  const count = await blague.count();
+  if (count === 0) {
+    await blague.bulkCreate(blagues);
+    console.log(" Blagues ajoutées automatiquement");
+  }
+
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
